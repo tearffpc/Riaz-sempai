66b0fbb7fd97597fb9051d02d57302d04a033237
N3XU5 51R <88531428+nexusNw@users.noreply.github.com>
2021-12-03 19:43:25 +0530

    update

--- plugins/admin.js
+++ plugins/admin.js
@@ -1601,8 +1601,7 @@ Riaz.addCommand({pattern: 'revoke', fromMe: true, desc: "Revokes/resets group's 
     await message.client.sendMessage(message.jid, "```Group link reset successfully ✅```", MessageType.text);
 	}}));
 
-Riaz.addCommand({pattern: 'rename ?(.*)', onlyGroup: true, fromMe: true,desc: 'change group name'}, (async (message, match) => {
-   if (message.jid.endsWith('@g.us')) {
+Riaz.addCommand({pattern: 'rename ?(.*)', onlyGroup: true, fromMe: true,desc: 'change group name ,works only in old grps'}, (async (message, match) => {
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,'i am not admin',MessageType.text);
     if (match[1] === '') return await message.client.sendMessage(message.jid,'give a name for you group \n exampple- .rename Riazgregory fans',MessageType.text);
